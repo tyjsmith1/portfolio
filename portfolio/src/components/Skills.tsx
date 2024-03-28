4480d28378bb4fc865dc4274007eddc3b85a8d76
tylersmith <tyjsmith1@gmail.com>
2024-03-28 13:49:42 -0500

    some light refactoring and code cleanup

--- portfolio/src/components/Skills.tsx
+++ portfolio/src/components/Skills.tsx
@@ -1,21 +1,35 @@
 "use client"
 
 import { SiJavascript, SiReact, SiTypescript, SiPython, SiFlask, SiPostgresql, SiNextdotjs, SiTailwindcss} from "react-icons/si";
+import { IconType } from "react-icons";
+
+interface Skill {
+    name: string;
+    icon: IconType;
+    colorClass?: string
+}
 
 export default function Skills() {
+    const skills: Skill[] = [
+        { name: "JavaScript", icon: SiJavascript, colorClass: "text-yellow-300"},
+        { name: "React", icon: SiReact, colorClass:"text-blue-300"},
+        { name: "Typescript", icon: SiTypescript, colorClass:"text-blue-800"},
+        { name: "Next.js", icon: SiNextdotjs},
+        { name: "Python", icon: SiPython, colorClass:"text-green-500"},
+        { name: "Flask", icon: SiFlask},
+        { name: "PostgreSQL", icon: SiPostgresql, colorClass:"text-blue-700"},
+        { name: "Tailwind CSS", icon: SiTailwindcss, colorClass:"text-teal-500"},
+    ]
+
     return (
         <div className="w-1/2 justify-start">
             <div className="px-6 space-y-4">
                 <h1 className="text-3xl font-bold text-end border-b border-gray-200">Familiar Technologies</h1>
                 <ul className="flex flex-wrap justify-end h-full">
-                    <li className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200"><SiJavascript className="text-yellow-300" size={60}/> JavaScript</li>
-                    <li className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200"><SiReact className="text-blue-300" size={60}/> React</li>
-                    <li className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200"><SiTypescript  className="text-blue-800" size={60}/> TypeScript</li>
-                    <li className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200"><SiNextdotjs  size={60}/> Next.js</li>
-                    <li className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200"><SiPython className="text-green-500" size={60}/> Python</li>
-                    <li className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200"><SiFlask  size={60}/> Flask</li>
-                    <li className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200"><SiPostgresql className="text-blue-700" size={60}/> PostgreSQL</li>
-                    <li className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200"><SiTailwindcss className="text-teal-500" size={60}/> Tailwind CSS</li>
+                    {skills.map(({ name, icon: Icon, colorClass}) => (
+                        <li key={name} className="py-2 flex flex-col items-center m-3 mt-5 text-2xl hover:scale-105 transition-transform duration-200">
+                            <Icon className={colorClass ?? ''} size={60}/> {name}</li>
+                    ))}
                 </ul>
             </div>
         </div>
--- portfolio/src/components/Socials.tsx
+++ portfolio/src/components/Socials.tsx
@@ -2,31 +2,31 @@
 
 import { FaGithub, FaLinkedin, FaYoutube } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
+import { IconType } from "react-icons";
+
+interface SocialLink {
+    url: string;
+    icon: IconType;
+}
 
 export default function Socials() {
+    const socialLinks: SocialLink[] = [
+        { url: "https://github.com/tyjsmith1", icon: FaGithub },
+        { url: "https://www.linkedin.com/in/tyler-smith-pm-se/", icon: FaLinkedin },
+        { url: "https://leetcode.com/tyjsmith1/", icon: SiLeetcode },
+        { url: "https://www.youtube.com/@tylersmith644", icon: FaYoutube },
+    ]
+
     return (
         <div>
             <ul className="flex flex-row">
-                <li className="px-2 hover:scale-110 transition-transform duration-200">
-                    <a href="https://github.com/tyjsmith1" target="_blank" rel="noopener noreferrer">
-                        <FaGithub size={30}/>
+                {socialLinks.map(({ url, icon: Icon}) => 
+                <li key={url} className="px-2 hover:scale-110 transition-transform duration-200">
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        <Icon size={30}/>
                     </a>
                 </li>
-                <li className="px-2 hover:scale-110 transition-transform duration-200">
-                    <a href="https://www.linkedin.com/in/tyler-smith-pm-se/" target="_blank" rel="noopener noreferrer">
-                        <FaLinkedin size={30}/>
-                    </a>
-                </li>
-                <li className="px-2 hover:scale-110 transition-transform duration-200">
-                    <a href="https://leetcode.com/tyjsmith1/" target="_blank" rel="noopener noreferrer">
-                        <SiLeetcode size={30}/>
-                    </a>
-                </li>
-                <li className="px-2 hover:scale-110 transition-transform duration-200">
-                    <a href="https://www.youtube.com/@tylersmith644" target="_black" rel="noopener noreferrer">
-                        <FaYoutube size={30}/>
-                    </a>
-                </li>
+                )}
             </ul>
         </div>
     )
